3d3e96d85c31347758394c5021c3700cc3132d25
OlalaTheExpert <olalavictor01@gmail.com>
2021-08-13 21:33:49 +0300

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+             
+        </h2>
+       
+    </x-slot>
+<x-slot name="main">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Messages') }}
+             
+        </h2>
+</x-slot>
+
+</x-app-layout>
